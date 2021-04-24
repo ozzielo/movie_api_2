ce6072bca9f314b55c5b9a722c8b1076595d8489
ozzielo <57558395+ozzielo@users.noreply.github.com>
2021-04-23 23:57:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/test',
-{ useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/test',
+// { useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.connect(' process.env.CONNECTION_URI',
 { useNewUrlParser: true, useUnifiedTopology: true});
