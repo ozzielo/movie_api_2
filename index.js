ebd82c9eeb2941543b9bf37dfec8532ec85522b0
ozzielo <57558395+ozzielo@users.noreply.github.com>
2021-04-24 00:05:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { check, validationResult } = require('express-validator');
 // mongoose.connect('mongodb://localhost:27017/test',
 // { useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect(' process.env.CONNECTION_URI',
+mongoose.connect('process.env.CONNECTION_URI',
 { useNewUrlParser: true, useUnifiedTopology: true});
 // const topMovies = [
 //   {
