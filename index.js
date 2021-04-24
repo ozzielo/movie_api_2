4eaecaa0790db2ebdc979ffb591d2743d18bc2de
ozzielo <57558395+ozzielo@users.noreply.github.com>
2021-04-24 17:24:50 -0400

    Revert "Update index.js"
    This reverts commit ff844c3eca65db300070e20bfac7a95af2ec0820.

--- index.js
+++ index.js
@@ -141,8 +141,7 @@ mongoose.connect('process.env.CONNECTION_URI',
 //     ]
 //   }
 // ];
-const cors = require('cors');
-app.use(cors());
+
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
