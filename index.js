6009661c136acb526c3a06f091869a9ccc21f4cc
ozzielo <57558395+ozzielo@users.noreply.github.com>
2021-04-26 17:18:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const cors = require('cors');
 app.use(cors());
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/test',
-{ useNewUrlParser: true, useUnifiedTopology: true});
-
+// mongoose.connect('mongodb://localhost:27017/test',
+// { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI,
+   { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
