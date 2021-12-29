4e639ae77b142cc47c1fa8304c91792eb75c0035
ozzielo <57558395+ozzielo@users.noreply.github.com>
2021-12-29 17:34:38 -0500

    added get user info

--- index.js
+++ index.js
@@ -168,13 +168,28 @@ app.delete('/users/:Username/Favorites/:MovieID', passport.authenticate('jwt', {
     { new: true },
     (err, updatedUser) => {
       if (err) {
-        consolle.error(err);
+        console.error(err);
         res.status(500).send('Error: ' + err);
       } else {
         res.json(updatedUser);
       }
     });
   // res.send('Deleted ________ from your favorites!');
+});
+
+
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {//error callback
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json('Hit Get')
 });
 
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
